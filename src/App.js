4d01db6b6550f8302eb706d1b9131b2a64226fa7
Prasanna Kumar Yalala <yprasannakumar@gmail.com>
2018-09-20 02:00:31 +0100

    attempt #1 at making carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import api from './api/pixabay';
-
+import arrowAssetPath from './assets/images/arrow.svg';
 import './App.scss';
 
 class App extends Component {
@@ -12,7 +12,7 @@ class App extends Component {
     api.getImages().then((data)=>{
       let carousel = data.map((item, key) => {
         return (
-          <div className="slide" style={{backgroundImage: `url(${item.webformatURL})`}}></div>
+          <div className={ "slide" + ( key === 0 ? ' active' : '') } style={{backgroundImage: `url(${item.webformatURL})`}}></div>
         );
       })
       this.setState({
@@ -25,8 +25,18 @@ class App extends Component {
   }
   render() {
     return (
-      <div className = "slider">
-        {this.state.imageListCarousel}
+      <div className = "slider-container">
+        <div className = "slider">
+          {this.state.imageListCarousel}
+        </div>
+        <div className = "slider-controls">
+          <div className = "button-container">
+            <div className = "btn prev" style={{backgroundImage: `url(${arrowAssetPath})`}} />
+          </div>
+          <div className = "button-container">
+            <div className = "btn next" style={{backgroundImage: `url(${arrowAssetPath})`}} />
+          </div>
+        </div>
       </div>
     );
   }
