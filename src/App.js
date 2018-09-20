ca5d3b556ccbe4e1c1da77d494295257871b2169
Prasanna Kumar Yalala <yprasannakumar@gmail.com>
2018-09-20 01:20:25 +0100

    refactored API, integrated API into core code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import api from './api/pixabay';
+
 import './App.scss';
 
 class App extends Component {
+  state = {
+    imageList: [],
+    imageListCarousel: void 0
+  };
+  componentDidMount() {
+    api.getImages().then((data)=>{
+      let carousel = data.map((item, key) => {
+        return (
+          <div className="slide" style={{backgroundImage: `url(${item.webformatURL})`}}></div>
+        );
+      })
+      this.setState({
+        imageList: data,
+        imageListCarousel: carousel
+      });
+    }).catch((err)=>{
+      throw new Error(err);
+    })
+  }
   render() {
     return (
-      <div className="App">
-        Hello World
+      <div className = "slider">
+        {this.state.imageListCarousel}
       </div>
     );
   }
--- src/api/pixabay.js
+++ src/api/pixabay.js
@@ -3,20 +3,21 @@ import axios from 'axios';
 const PIXABAY_API_URL = '//pixabay.com/api/';
 const PIXABAY_API_KEY = '9656065-a4094594c34f9ac14c7fc4c39';
 const PIXABAY_IMAGE_TYPE = 'photo';
+const queryString = 'beautiful+landscape';
 
-class API {
-  constructor () {
-    let queryString = 'beautiful+landscape';
-    this.API_URL = `${PIXABAY_API_URL}?key=${PIXABAY_API_KEY}&q=${queryString}&image_type=${PIXABAY_IMAGE_TYPE}`;
-  }
-
-  getImages () {
-      return axios.get(this.API_URL).then((response)=>{
-        // TBA
-      },(response) => {
-        throw new Error("An error has occurred: ", response);
-      });
-  }
+function getImages() {
+  let apiUrl = `${PIXABAY_API_URL}?key=${PIXABAY_API_KEY}&q=${queryString}&image_type=${PIXABAY_IMAGE_TYPE}`;
+  return axios.get(apiUrl)
+    .then((response) => {
+      if (response.data && (response.data.hits && response.data.hits.length > 0)) {
+        return response.data.hits;
+      } else {
+        throw new Error("No data found!");
+      }
+    })
+    .catch((response) => {
+      throw new Error("An error has occurred: ", response);
+    });
 }
 
-export default API;
+export default { getImages };
